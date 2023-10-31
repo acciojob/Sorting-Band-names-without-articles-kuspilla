544e38aa07ebd735c2b9a37ef7ef24b229d02a3e
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-10-31 05:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
-let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Pink Floyd'];
+// This file is not to be modified. Please ignore this.
+// We will understand all of this later in the course.
+// DO NOT MODIFY THIS FILE
 
-document.addEventListener("DOMContentLoaded", function () {
-    const sortedBands = sortBandNamesWithoutArticles(bandNames);
-    const ul = document.getElementById('band');
+const express = require('express');
+const path = require('path');
 
-    sortedBands.forEach(band => {
-        const li = document.createElement('li');
-        li.textContent = band;
-        ul.appendChild(li);
-    });
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
 });
-
-
-function sortBandNamesWithoutArticles(bandNames) {
-    // Define an array of common articles to be excluded
-    const articles = ['a', 'an', 'the'];
-
-    // Remove articles and convert to lowercase for sorting
-    const sortedBandNames = bandNames.map(band => {
-        const words = band.split(' ');
-        const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
-        return filteredWords.join(' ').toLowerCase();
-    });
-
-    // Sort the modified band names in lexicographic order
-    sortedBandNames.sort();
-
-    // Restore the original case and format
-    const result = sortedBandNames.map(sortedName => {
-        return bandNames.find(band => sortedName === band.toLowerCase().replace(/(a|an|the)\s/, ''));
-    });
-
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
