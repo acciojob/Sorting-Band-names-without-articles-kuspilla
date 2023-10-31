67c75c63be0984fc1e8a3523f46f05c43bab1530
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-10-31 05:59:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-// Array of band names
-let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'Aerosmith', 'The Beatles', 'Pink Floyd'];
+//your code here
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-// Function to remove articles from a band name
-function removeArticles(name) {
-  return name.replace(/^(a|an|the)\s+/i, '');
-}
+arr.sort();
+let ans=[];
 
-// Sort the band names without articles
-bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
-
-// Get the UL element with id 'band'
-const bandList = document.getElementById('band');
-
-// Create list items and add them to the UL
-bandNames.forEach(name => {
-  const listItem = document.createElement('li');
-  listItem.textContent = name;
-  bandList.appendChild(listItem);
-});
+console.log(ans);
